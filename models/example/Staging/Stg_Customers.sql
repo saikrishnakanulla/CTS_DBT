a491f27404f9effc9733b39c3dc54a42b177215a
kanulla.saikrishna@cognizant.com <kanulla.saikrishna@cognizant.com>
2025-01-31 12:22:15 +0000

    Commit

--- models/example/Staging/Stg_Customers.sql
+++ models/example/Staging/Stg_Customers.sql
@@ -2,4 +2,6 @@
 
 --select *from qwt_analytics.raw_dev.raw_customers
 
-select *from {{source('raw_qwt','raw_customers')}}
+--select *from {{source('raw_qwt','raw_customers')}}
+
+select *from qwt_analytics.raw_dev.raw_products
